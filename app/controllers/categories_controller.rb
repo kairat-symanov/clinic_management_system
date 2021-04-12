57ec13f45dce048bae97cf5833e65eafaa311c5f
Kairat Symanov <symanov79@gmail.com>
2021-04-12 21:46:18 +0600

    Add new application

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,49 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.delete
+    redirect_to categories_path
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:title, :description)
+  end
+
+end
