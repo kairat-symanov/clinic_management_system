15cfb31eebf3038aa0a4f8e206a8f63fa8d94ed3
Kairat Symanov <symanov79@gmail.com>
2021-09-29 22:39:41 +0600

    clinic_5 - added validation in model Category and changes were made to views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+
   def index
     @categories = Category.all
   end
@@ -47,3 +48,4 @@ class CategoriesController < ApplicationController
   end
 
 end
+ 
